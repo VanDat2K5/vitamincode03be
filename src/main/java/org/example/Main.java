281cf70353dbbad352d564748ae331616c571cae
VanDat2K5 <vandat05022005@gmail.com>
2025-05-17 16:10:54 +0700

    Commit 01: Add new print in main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello Vitamincode 03 - Git!");
+        System.out.println("Hello Admin");
     }
 }
